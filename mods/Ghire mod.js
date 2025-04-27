ed6979e6c275a4734f505763529662dd3493625a
personiguess-a <slitherlightalt@gmail.com>
2025-04-27 14:39:53 -0400

    Create Ghire mod.js

---
+++ mods/Ghire mod.js
@@ -0,0 +1,78 @@
+// Adding elements:
+elements.Ghire = {
+    color: "#403066",
+    behavior: behaviors.GAS,
+    category: "gases",
+    state: "gas"
+    hidden:false
+    
+};
+
+// Changing existing elements:
+elements.water.color = "#ff0000";
+elements.water.behavior = behaviors.WALL;
+
+// Removing elements:
+// Be aware, things may break
+delete elements.ketchup;
+
+// Adding behavior presets:
+behaviors.SELFDELETE = [
+    "XX|XX|XX",
+    "XX|DL|XX",
+    "XX|XX|XX",
+];
+
+// Raw JavaScript behaviors:
+behaviors.mud.tick = function(pixel) {
+    if (tryMove(pixel, pixel.x, pixel.y+1)) {
+        console.log("Moved!");
+    }
+    else {
+        console.log("Couldn't move!")
+    }
+};
+
+// Create a new tool:
+elements.sand_exploder = {
+    color: "#ff0000",
+    tool: function(pixel) {
+        if (pixel.element == "sand") {
+            pixel.element = "explosion"
+        }
+    },
+    category: "tools",
+};
+
+// Add reactions to existing elements
+if (!elements.water.reactions) { // Include this block once
+    elements.water.reactions = {} // This creates the property if it doesn't exist
+}
+elements.water.reactions.mayo = { "elem1":null, "elem2":"mayo_water" }
+elements.water.reactions.soap = { "elem1":null, "elem2":"soapy_water" }
+
+// Run after all mods are loaded, for cross-mod compatibility
+runAfterLoad(function() {
+    elements.Ghire = {
+    color: "#403066",
+    behavior: behaviors.GAS,
+    category: "gases",
+    state: "gas"
+    hidden:false
+    
+};// Your code here
+    console.log("Hello World!");
+});
+
+// Run if another mod is active
+if (enabledMods.includes("mods/test.js")) {
+    runAfterLoad(function() {
+        // Your code here
+        console.log("Hello World!");
+    });
+}
+
+// Creating eLists:
+eLists.CONDIMENT = ["ketchup","melted_cheese","mayo"];
+// Adding elements to eLists:
+eLists.CONDIMENT.push("ghire");
